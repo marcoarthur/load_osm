ead8f162e4f2a3b11ee1a3694e4721322c51796e
Marco Arthur <arthurpbs@gmail.com>
2019-08-09 01:10:12 -0300

    Add schemas definitions

---
+++ revert/schema_defs.sql
@@ -0,0 +1,22 @@
+-- Revert load_brasil_osm:schema_defs from pg
+
+BEGIN;
+
+-- Drop all tables if they exist.
+DROP TABLE IF EXISTS actions;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS nodes;
+DROP TABLE IF EXISTS node_tags;
+DROP TABLE IF EXISTS ways;
+DROP TABLE IF EXISTS way_nodes;
+DROP TABLE IF EXISTS way_tags;
+DROP TABLE IF EXISTS relations;
+DROP TABLE IF EXISTS relation_members;
+DROP TABLE IF EXISTS relation_tags;
+DROP TABLE IF EXISTS schema_info;
+
+-- Drop all stored procedures if they exist.
+DROP FUNCTION IF EXISTS osmosisUpdate();
+
+
+COMMIT;
---
+++ verify/schema_defs.sql
@@ -0,0 +1,16 @@
+-- Verify load_brasil_osm:schema_defs on pg
+
+BEGIN;
+
+	select * from schema_info;
+	select * from users;
+	select * from nodes;
+	select * from node_tags;
+	select * from ways;
+	select * from way_nodes;
+	select * from way_tags;
+	select * from relations;
+	select * from relation_members;
+	select * from relation_tags;
+
+ROLLBACK;
