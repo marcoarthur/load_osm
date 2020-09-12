52093f4d1d719b48d51df307c571111a96b7ed4a
Marco Arthur <arthurpbs@gmail.com>
2020-09-12 11:20:26 -0300

    Add test to map_url()

---
+++ t/functions.sql
@@ -0,0 +1,14 @@
+BEGIN; 
+SELECT no_plan(); 
+
+PREPARE call_map_url AS SELECT osm_brasil.map_url('ways', 10);
+PREPARE map_url_result  AS VALUES ('https://www.openstreetmap.org/?ways=10'::text);
+
+SELECT results_eq( 'call_map_url', 
+    'map_url_result', 'osm_brasil.map_url()' );
+
+
+SELECT finish(); 
+ROLLBACK;
+
+-- dbext:type=PGSQL:user=devel:host=127.0.0.1:dbname=make_test
