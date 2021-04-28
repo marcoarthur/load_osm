465ca90bc126b2a7ef22a8cfa757e02da7dcba42
Marco Arthur <arthurpbs@gmail.com>
2021-04-27 21:57:41 -0300

    Fix
    Extract jsonb field with ->> operator so it transform it into text.
    Without that id continues being jsonb and that cause where statement
    to fail.

--- deploy/ibge_sectors.sql
+++ deploy/ibge_sectors.sql
@@ -7,10 +7,10 @@ BEGIN;
     COPY t_aux FROM PROGRAM 'curl https://gist.githubusercontent.com/marcoarthur/2f0fde0972363c4344659ad7f06a1267/raw/c1b6fcd7cfdc4f44077c2cb75572e8f1b7658a6f/regioes_sensitarias_ubatuba.geojson';
 
     -- Extract information from json and save it on table ibge_sectors
-    SELECT properties->'name' AS id, ST_GeomFromGeoJson(geometry) AS geom
+    SELECT properties->>'name' AS id, ST_GeomFromGeoJson(geometry) AS geom
     INTO TABLE osm_brasil.ibge_sectors
     FROM t_aux, jsonb_to_recordset(t_aux.j->'features') AS ele(type text, geometry text, properties jsonb);
-
+    
 COMMIT;
 
 -- dbext:type=PGSQL:user=devel:host=127.0.0.1:dbname=make_test
