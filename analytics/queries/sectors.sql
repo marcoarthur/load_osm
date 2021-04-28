ebdbb1b16c07ed225cb17926ff826bb449be7c9c
Marco Arthur <arthurpbs@gmail.com>
2021-04-28 18:23:18 -0300

    Basic analytic

--- analytics/queries/sectors.sql
+++ analytics/queries/sectors.sql
@@ -12,10 +12,6 @@ FROM sector_areas
 ORDER BY area_sqkm;
 
 --
--- Sectors and Node counts 
---
-
---
 -- River crossing sectors
 --
 
@@ -29,7 +25,26 @@ WITH rivers_sectors AS (
             ST_Crosses(ibs.geom, w.linestring)
       ORDER BY length
 ) SELECT url, array_agg(sector_id) as sectors
-from rivers_sectors
-group by url;
+FROM rivers_sectors
+GROUP BY url;
+
+--
+-- Tagged nodes by sectors
+--
+
+WITH tagged_nodes_by_sector AS (
+      SELECT ib.id AS sector_id,
+             ST_Area(ST_Transform(ST_MakePolygon(ib.geom), 26986))/10^6 AS sector_area_km,
+             ib.geom,
+             count(*) AS total_tagged
+      FROM osm_brasil.nodes n
+           RIGHT JOIN osm_brasil.ibge_sectors ib ON
+           ST_WITHIN(n.geom, ST_MakePolygon(ib.geom))
+      WHERE n.tags <> ''
+      GROUP BY ib.id, ib.geom
+)
+SELECT sector_id, sector_area_km, total_tagged / sector_area_km  AS density
+FROM tagged_nodes_by_sector
+ORDER BY density DESC;
 
 -- dbext:type=PGSQL:user=devel:host=127.0.0.1:dbname=gis
